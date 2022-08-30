54405190ceb56728fdc94fd69a2e80baaafd9e40
Alexander Howard <you@example.com>
2022-08-29 22:47:45 -0400

    2nd commit being made

---
+++ src/Lab1.java
@@ -0,0 +1,36 @@
+
+public class Lab1 {
+
+  public static void main(String[] args) {
+      int availablePlaneSeats = 5;
+      
+      double costOfGroceries  = 59.95; 
+      
+      char middleInitial = 'M';
+      
+      boolean isHotOutside = false;
+      
+      String firstNameIs = "Alex";
+      
+      String streetAddress = "2307 NW 27th Terrace, Fort Myers, FL 33905"; 
+      
+      System.out.println("Available seats left on the plane: " + availablePlaneSeats);
+      System.out.println("$" + costOfGroceries + " is the cost of groceries");
+      System.out.println("The person's Middle initial is " + middleInitial);
+      System.out.println(isHotOutside);
+      System.out.println("the customer's first name is " + firstNameIs);
+      System.out.println(streetAddress + " is where the person lives.");
+      
+      
+      
+      
+      
+      
+   
+    
+    
+    
+    
+  }
+
+}
